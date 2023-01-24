9e6987dfb67cc3772f5e52ca64d473ff3dc0f456
Sava Ivanov <59567777+SavaIv@users.noreply.github.com>
2023-01-24 22:27:10 +0200

    Open AboutUs and Blog pages for building

---
+++ LawOfficeAdvertisement/Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LawOfficeAdvertisement.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult ListOfAllActivities()
+        {
+            return View();
+        }
+    }
+}
