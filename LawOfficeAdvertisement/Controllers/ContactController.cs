6652c9349351b8d80eee944c85c9fd1b97e4f419
Madlena Ivanova <madlena.iv@abv.bg>
2023-01-24 18:04:16 +0200

    Changed navbar and topbar colours and size. Added page Contacts

---
+++ LawOfficeAdvertisement/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LawOfficeAdvertisement.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+    }
+}
