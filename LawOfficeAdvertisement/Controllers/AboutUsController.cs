820894ea82dfddeae8e7893f4545ed197466f981
NikkiIV <prince_the_ripper@abv.bg>
2023-01-26 13:26:53 +0200

    Added pages and views to buttons in AboutUs

--- LawOfficeAdvertisement/Controllers/AboutUsController.cs
+++ LawOfficeAdvertisement/Controllers/AboutUsController.cs
@@ -8,5 +8,43 @@ namespace LawOfficeAdvertisement.Controllers
         {
             return View();
         }
+        public IActionResult AdministrativnoPravo()
+        {
+            return View();
+        }
+
+        public IActionResult KorporativnoPravo()
+        {
+            return View();
+        }
+
+        public IActionResult NakazatelnoPravo()
+        {
+            return View();
+        }
+        public IActionResult NesastoyatelnostLikvidaciya()
+        {
+            return View();
+        }
+        public IActionResult ObligacionnoPravo()
+        {
+            return View();
+        }
+        public IActionResult ProcesualnoPredstavitelstvo()
+        {
+            return View();
+        }
+        public IActionResult SemeynoNasledstvenoPravo()
+        {
+            return View();
+        }
+        public IActionResult TargovskoPravo()
+        {
+            return View();
+        }
+        public IActionResult VeshtnoPravo()
+        {
+            return View();
+        }
     }
 }
